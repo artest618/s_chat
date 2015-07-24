a344ff2a87842c30e0973b96029fb39bf1e8ea1a
gardenzjd <gardenzjd@163.com>
2015-07-24 16:30:41 +0800

    modified the user services

--- server/services/user.js
+++ server/services/user.js
@@ -4,8 +4,17 @@ var UserService = {
     checkuser: function(uid, callback){
         var sql = 'SELECT * FROM TB_USERINFO WHERE UID=' + uid;
         JDB.query(sql,function(err,vals,fields){
-            console.log(JSON.stringify(vals));
+            //console.log(JSON.stringify(vals));
+            if(err){
+                console.log(JSON.stringify(err));
+            }
+            callback(vals);
         });
     },
-
+    checkuserByName: function(name, callback){
+        var sql = 'SELECT * FROM TB_USERINFO WHERE NAME=\'' + name + '\'';
+        JDB.query(sql, function(err, vals, fields){
+            callback(vals);
+        });
+    }
 }
