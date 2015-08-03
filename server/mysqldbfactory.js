4dd47c898cbe3f5f14191c65cbf8a6a89a0cb71c
gardenzjd <gardenzjd@163.com>
2015-08-03 17:14:04 +0800

    sign

--- server/mysqldbfactory.js
+++ server/mysqldbfactory.js
@@ -4,7 +4,7 @@ var mysql=require("mysql");
 var pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: '123456',
+    password: '1q2w3e4r',
     database: 'chat',
     port: 3306
 });
