ee0ae3e90be1a346c0945ea06be4d0c7e0c91334
gardenzjd <gardenzjd@163.com>
2015-08-09 15:40:06 +0800

    contactlist

--- server/services/counselor.js
+++ server/services/counselor.js
@@ -16,7 +16,7 @@ var CounselorService = {
      * @param callback
      */
     queryCounselor:function(userInfo,callback){
-        var sql = 'SELECT * FROM TB_USERINFO WHERE UID=\'' + userInfo.uid + '\'';
+        var sql = 'SELECT * FROM tb_userinfo WHERE uid=\'' + userInfo.uid + '\'';
         JDB.query(sql, function(err, vals, fields){
             callback(vals);
         });
@@ -28,7 +28,7 @@ var CounselorService = {
      */
     createCounselor: function(userInfo, callback){
         var self= this;
-        var sql = "INSERT INTO TB_USERINFO (uid, name, cname, usertype, groupcount, createdate)  " +
+        var sql = "INSERT INTO tb_userinfo (uid, name, cname, usertype, groupcount, createdate)  " +
                   "VALUES ("+userInfo.uid+", '"+userInfo.uname+"', '"+userInfo.uname+"', 3, 3, '"+_util.dateFormat("yyyy-MM-dd hh:mm:ss")+"')";
         var msg = "创建成功",msg_error = "顾问已存在，请不要重复创建";
             this.queryCounselor(userInfo,function(vals){
@@ -75,9 +75,9 @@ var CounselorService = {
         //admin_id 为顾问群的拥有者的ID暂定为1,根据这个id查询组信息
         self.queryGroupByOwer(admin_id,function(group){
 
-            sql =["INSERT INTO TB_GROUPLIST (owner, ownername, ownercname, groupname, grouptype, groupnum) VALUES ("+userInfo.uid+", '"+userInfo.uname+"', '"+userInfo.uname+"', '"+(userInfo.uname+"的客户群")+"', 2, '"+(userInfo.uid+"_"+Math.floor(Math.random()*(10000000000-1000+1)+1000))+"')",
-                "INSERT INTO TB_GROUPLIST (owner, ownername, ownercname, groupname, grouptype, groupnum) VALUES ("+userInfo.uid+", '"+userInfo.uname+"', '"+userInfo.uname+"', '"+(userInfo.uname+"的客户经理群")+"',3, '"+(userInfo.uid+"_"+Math.floor(Math.random()*(10000000000-1000+1)+1000))+"')",
-                "INSERT INTO TB_GROUP_USERLIST (userid, username, usercname, usertype, groupid, jointime) VALUES ("+userInfo.uid+", '"+userInfo.uname+"', '"+userInfo.uname+"', 3,'"+group[0].id+"', '"+_util.dateFormat("yyyy-MM-dd hh:mm:ss")+"')"
+            sql =["INSERT INTO tb_grouplist (owner, ownername, ownercname, groupname, grouptype, groupnum) VALUES ("+userInfo.uid+", '"+userInfo.uname+"', '"+userInfo.uname+"', '"+(userInfo.uname+"的客户群")+"', 2, '"+(userInfo.uid+"_"+Math.floor(Math.random()*(10000000000-1000+1)+1000))+"')",
+                "INSERT INTO tb_grouplist (owner, ownername, ownercname, groupname, grouptype, groupnum) VALUES ("+userInfo.uid+", '"+userInfo.uname+"', '"+userInfo.uname+"', '"+(userInfo.uname+"的客户经理群")+"',3, '"+(userInfo.uid+"_"+Math.floor(Math.random()*(10000000000-1000+1)+1000))+"')",
+                "INSERT INTO tb_group_userlist (userid, username, usercname, usertype, groupid, jointime) VALUES ("+userInfo.uid+", '"+userInfo.uname+"', '"+userInfo.uname+"', 3,'"+group[0].id+"', '"+_util.dateFormat("yyyy-MM-dd hh:mm:ss")+"')"
             ] ;
 
             JDB.oper(sql, function(result){
@@ -85,8 +85,8 @@ var CounselorService = {
                 self.queryGroupByOwer(userInfo.uid,function(group){
 
                     sql =[
-                        "INSERT INTO TB_GROUP_USERLIST (userid, username, usercname, usertype, groupid, jointime) VALUES ("+userInfo.uid+", '"+userInfo.uname+"', '"+userInfo.uname+"', 3,'"+group[0].id+"', '"+_util.dateFormat("yyyy-MM-dd hh:mm:ss")+"')",
-                        "INSERT INTO TB_GROUP_USERLIST (userid, username, usercname, usertype, groupid, jointime) VALUES ("+userInfo.uid+", '"+userInfo.uname+"', '"+userInfo.uname+"', 3,'"+group[1].id+"', '"+_util.dateFormat("yyyy-MM-dd hh:mm:ss")+"')"
+                        "INSERT INTO tb_group_userlist (userid, username, usercname, usertype, groupid, jointime) VALUES ("+userInfo.uid+", '"+userInfo.uname+"', '"+userInfo.uname+"', 3,'"+group[0].id+"', '"+_util.dateFormat("yyyy-MM-dd hh:mm:ss")+"')",
+                        "INSERT INTO tb_group_userlist (userid, username, usercname, usertype, groupid, jointime) VALUES ("+userInfo.uid+", '"+userInfo.uname+"', '"+userInfo.uname+"', 3,'"+group[1].id+"', '"+_util.dateFormat("yyyy-MM-dd hh:mm:ss")+"')"
                     ] ;
 
                     JDB.oper(sql, function(result){
@@ -107,7 +107,7 @@ var CounselorService = {
      * @param callback
      */
     queryGroupByOwer:function(ower_id,callback){
-        var sql = 'SELECT * FROM TB_GROUPLIST WHERE OWNER=\'' + ower_id + '\'';
+        var sql = 'SELECT * FROM tb_grouplist WHERE owner=\'' + ower_id + '\'';
         JDB.query(sql, function(err, vals, fields){
             callback(vals);
         });
