c469afa0d4bf5df76d6b6da762ad0191afcd0c21
gardenzjd <gardenzjd@163.com>
2015-08-21 10:03:58 +0800

    聊天记录分页bug修改

--- server/mysqldbfactory.js
+++ server/mysqldbfactory.js
@@ -2,8 +2,8 @@ var mysql=require("mysql");
 //var queues = require('mysql-queues');
 
 var pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
+    host: 'chat.rdsmpk3228qyv2t.rds.bj.baidubce.com',
+    user: 'zjd4chat',
     password: '1q2w3e4r',
     database: 'chat',
     port: 3306
