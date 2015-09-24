96c57f5205d9fc46fd5ecf4a313499dff75e531d
tuzhao0206 <system>
2015-09-25 04:12:21 +0800

    Merge remote-tracking branch 'origin/dev' into dev
    Conflicts:
    	chat.sql
    	myapp.js

--- client/js/schat.js
+++ client/js/schat.js
@@ -87,6 +87,25 @@ require(['jquery', 'common', 'domReady', 'ejs', 'AjaxUpload'], function($, Commo
         //});
     });
 
+    function windowclose() {
+        var browserName = navigator.appName;
+        if (browserName=="Netscape") {
+            window.open('', '_self', '');
+            window.close();
+        }
+        else {
+            if (browserName == "Microsoft Internet Explorer"){
+                window.opener = "whocares";
+                window.opener = null;
+                window.open('', '_top');
+                window.close();
+            }
+        }
+    }
+
+
+
+
     function initChatList(){
         Common.post({
             url: 'chatList',
